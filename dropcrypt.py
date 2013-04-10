a273ef629c4630a967dbd813ec2d714670b7afa9
Mattis Zbigniew Haase <madmaxx@madmaxx.info>
2013-04-10 13:09:33 +0200

    Code still doesnt work, but at least it should now be PEP-8 compatible

--- dropcrypt.py
+++ dropcrypt.py
@@ -1,108 +1,248 @@
 """
 ALPHA VERSION
 
-UNTESTED! SHOULD NOT BE TRUSTED FOR IMPORTANT DATA!
+UNTESTED! SHOULD NOT BE TRUSTED FOR 
+IMPORTANT DATA!
 
-This is the main script. Syncs files on first startup. Monitors 
-changes in local and dropbox folders, and then either decrypts or 
-encrypts the changed files, also moves, creates or deletes files 
-and folders, so both folders stay synced.
-Utilizes watchdog module.
+This is the main script. Syncs files on first startup. 
+Monitors changes in source and destination folders, 
+and then either decrypts or encrypts the changed 
+files, also moves, creates or deletes files and folders, 
+so both folders stay synced. Utilizes watchdog 
+module.
 
 Copyright 2013 Mattis Zbigniew Haase
 
 This file is part of Dropcrypt.
 
-    Dropcrypt is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+Dropcrypt is free software: you can redistribute it 
+and/or modify it under the terms of the GNU General 
+Public License as published by the Free Software 
+Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    Dropcrypt is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+Dropcrypt is distributed in the hope that it will be 
+useful, but WITHOUT ANY WARRANTY; without even 
+the implied warranty of MERCHANTABILITY or 
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Dropcrypt.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General
+Public License along with Dropcrypt.  If not, see 
+<http://www.gnu.org/licenses/>.
     
-This module utilizes the watchdog module, copyright 2010 by 
-gora kargosh http://pythonhosted.org/watchdog/
+This module utilizes the watchdog module, copyright
+2010 by gora kargosh 
+http://pythonhosted.org/watchdog/
 See watchdog_LICENSE for further details
 """
 
-import time,  configuration,  logging,  encryption,  os,  shutil
+import time
+import logging
+import hashlib
+import os
+import shutil
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-class Handler (FileSystemEventHandler):
+from watchdog.utils.dirsnapshot import DirectorySnapshot,  DirectorySnapshotDiff
+
+import configuration
+import encryption
+
+
+class _Handler (FileSystemEventHandler):
     """Acts on file system events"""
+    def on_created(self,  event):
+        """Creates folders, files are handled through
+        on_modified"""
+        if (event.src_path.startswith
+            (sourcepath)): 
+            if event.is_directory:
+                os.makedirs(event.src_path.replace
+                (sourcepath, destinationpath))
+        if (event.src_path.startswith
+            (destinationpath)): 
+            if event.is_directory:
+                os.makedirs(event.src_path.replace
+                (destinationpath, sourcepath))
+    
     def on_deleted(self,  event):
-        return
+        """
+        Detects and acts upon deletion events. for 
+        some reason it doesnt work yet, so deleted
+        files are only deleted upon restarting the script
+        """
+        filename = os.path.basename(event.src_path)
+        if event.src_path.startswith(sourcepath): 
+            path = event.src_path.replace(sourcepath, 
+                                          destinationpath)
+            #if something in sourcefolder was changed
+            if not os.path.exist(path):
+                #if there is no file to be deleted
+                return
+            else:
+                print ("deleting %s in destination" % 
+                       (filename))
+                os.remove(path)
+        if event.src_path.startswith(destinationpath): 
+            path = event.src_path.replace(destinationpath, 
+                                          sourcepath)
+            #if something in the destination was changed
+            if not os.path.exist(path):
+                #if there is no file to be deleted
+                return
+            else:
+                print ("deleting %s in source" % 
+                       (filename))
+                os.remove(path)
+    
     def on_modified(self, event):
-        """Syncs both folders. encrypts and decrypts files, creates 
-        folders"""
-        print "modified"
+        """acts upon modified files, calls encryption 
+        and decryption"""
         if not event.is_directory:
             #encryption and decryption of files below
             filename = os.path.basename(event.src_path)
-            dropboxpath = os.path.realpath("%s%s" % (configuration.dropbox, filename))
-            localpath = os.path.realpath("%s%s" % (configuration.local, filename))
-            if event.src_path.startswith(os.path.realpath(configuration.local)): 
-                #if local folder was changed
-                if os.path.isfile(dropboxpath) and ((os.path.getmtime(dropboxpath)-os.path.getmtime(localpath)) < 0): 
-                    #if there already is a file with the same name in dropboxfolder, and db file is younger
-                    print "return" 
+            if event.src_path.startswith(sourcepath): 
+                #if source folder was changed
+                path = event.src_path.replace(sourcepath, 
+                                              destinationpath)
+                if (os.path.isfile(destinationpath) and 
+                ((os.path.getmtime(destinationpath) -
+                  os.path.getmtime(sourcepath)) < 0)): 
+                    """if there already is a file with the same 
+                    name in destinationfolder, and db file 
+                    is younger"""
+                    print "file already up to date" 
                     return
                 else:
-                    print "encrypt"
-                    encryption.Encrypt(configuration.chunksize,  configuration.key,  localpath,  dropboxpath)
-            if event.src_path.startswith(os.path.realpath(configuration.dropbox)):
-                #if dropboxfolder was changed
-                if os.path.isfile(localpath) and ((os.path.getmtime(dropboxpath)-os.path.getmtime(localpath)) > 0):
-                    #if there already is a file called like this in the local folder, and local file is younger
-                    print "return"
+                    print ("encrypting %s" % (filename))
+                    encryption.Encrypt(event.src_path,
+                                       path,  key)
+            if event.src_path.startswith(destinationpath):
+                #if destinationfolder was changed
+                path = event.src_path.replace(
+                                    destinationpath, sourcepath)
+                if (os.path.isfile(sourcepath) and 
+                ((os.path.getmtime(destinationpath) -
+                  os.path.getmtime(sourcepath)) > 0)):
+                    #if there already is a file called like 
+                    #this in the source folder, and source 
+                    #file is younger
+                    print "file already up to date"
                     return
                 else:
-                    print "decrypt"
-                    encryption.Decrypt(configuration.chunksize,  configuration.key,  localpath,  dropboxpath)
+                    print ("decrypting %s" % (filename))
+                    encryption.Decrypt(path, 
+                                       event.src_path,  key)
         else:
             return
-            
+    
     def on_moved(self,  event):
-        print "moved"
-        if event.src_path.startswith(os.path.realpath(configuration.dropbox)):
-            #if a file was moved on the dropbox
-            #we replace the /home/bla/dropbox/ with home/bla/local/
-            src_path = event.src_path.replace(os.path.realpath(configuration.dropbox),  os.path.realpath(configuration.local))
-            dest_path = event.dest_path.replace(os.path.realpath(configuration.dropbox),  os.path.realpath(configuration.local))
+        if event.src_path.startswith(destinationpath):
+            #if a file was moved on the destination
+            #we replace the /home/bla/destination/ with
+            #home/bla/source/
+            src_path = event.src_path.replace(
+                                    destinationpath,  sourcepath)
+            dest_path = event.dest_path.replace(
+                                    destinationpath,  sourcepath)
             if os.path.exists(src_path):
-                #final check if the file or directory actually exists
-                #shazam
+                #final check if the file or directory 
+                #actually exists
+                print ("moving %s to %s"%(src_path,
+                                          dest_path))
                 shutil.move(src_path,  dest_path)
-        if event.src_path.startswith(os.path.realpath(configuration.local)):
-            #if a file was moved on /local
-            src_path=event.src_path.replace(os.path.realpath(configuration.local),  os.path.realpath(configuration.dropbox))
-            dest_path=event.dest_path.replace(os.path.realpath(configuration.local),  os.path.realpath(configuration.dropbox))
+        if event.src_path.startswith(sourcepath):
+            #if a file was moved on /source
+            src_path=event.src_path.replace(
+                                    sourcepath,  destinationpath)
+            dest_path=event.dest_path.replace(
+                                    sourcepath,  destinationpath)
             if os.path.exists(src_path):
-                #final check if the file or directory actually exists
-                #shazam
+                #final check if the file or directory 
+                #actually exists
+                print ("moving %s to %s"%(src_path,
+                                          dest_path))
                 shutil.move(src_path,  dest_path)
         else:
             return
+          
+def get_key():
+    """
+    gets key and hashes it a thousand times. Hashing
+    is used to
+    a) make sure the result is a 32 byte string, as this 
+    is needed for AES and 
+    b) make password more difficult to brute force if
+    attacker does memory dump.
+    
+    Unfortunately, in reality attackers either read 
+    hash from RAM and use this directly with a little
+    basic programming, or just use a keylogger. 
+    You should always follow this simple rule:
+    
+    if an attacker gains physical access, the machine
+    and everything on it is corrupted, and all secrets 
+    are revealed
+    """
+    global key
+    if configuration.keyfile=="":
+        key = input("Please enter your password: ")
+    else:
+        with open (keypath,  'r') as keyfileobj:
+            key = keyfileobj.read()
+            key = key.strip()
+    for i in range (0, 999):
+       key = hashlib.sha256(key).digest()
+        
+def sync():
+    """
+    syncs two directories. called once on start.
+    """
+    destinationsnapshotobj = DirectorySnapshot(
+                                               destinationpath, True)
+    sourcesnapshotobj = DirectorySnapshot(
+                                               sourcepath,  True)
+    diffobj = DirectorySnapshotDiff(
+            destinationsnapshotobj,  sourcesnapshotobj)
             
+    Lfiles_created = diffobj.files_created
+    Lfiles_moved = diffobj.files_moved
+    Lfiles_deleted = diffobj.files_deleted
+    Lfiles_modified = diffobj.files_modified
+    Ldirs_created = diffobj.dirs_created
+    Ldirs_moved = diffobj.dirs_moved
+    Ldirs_deleted = diffobj.dirs_deleted
+    Ldirs_modified = diffobj.dirs_modified
+
+
 def main():
-    while 1:
-        event_handler = Handler()
-        observer = Observer()
-        observer.schedule(event_handler, path=os.path.realpath(configuration.local), recursive=True)
-        observer.schedule(event_handler, path=os.path.realpath(configuration.dropbox), recursive=True)
-        observer.start()
-        try:
-            while 1:
-                time.sleep(1)
-        except keyboardInterrupt:
-               observer.stop()
-        observer.join()
+    global destinationpath
+    global sourcepath
+    global keypath
+    if not configuration.keyfile == "":
+        keypath = os.path.realpath(
+                                configuration.keyfile)
+    destinationpath = os.path.realpath(
+                                configuration.destination)
+    sourcepath = os.path.realpath(
+                                configuration.source)
+    get_key()
+    sync()
+    event_handler = _Handler()
+    observer = Observer()
+    observer.schedule(event_handler, 
+                      path=sourcepath, recursive=True)
+    observer.schedule(event_handler, 
+                      path=destinationpath, recursive=True)
+    observer.start()
+    try:
+        while 1:
+            time.sleep(1)
+    except keyboardInterrupt:
+           observer.stop()
+    observer.join()
         
 if __name__ == '__main__':
     main()
--- encryption.py
+++ encryption.py
@@ -1,103 +1,96 @@
 """
 ALPHA VERSION
 
-UNTESTED! SHOULD NOT BE TRUSTED FOR IMPORTANT DATA!
+UNTESTED! SHOULD NOT BE TRUSTED FOR 
+IMPORTANT DATA!
 
-This module provides encryption and decryption functions. 
+This module provides encryption and decryption 
+functions. 
 
-Encryption is done via AES. Note that the AES implementation 
-comes from the pycrypto module. 
+Encryption is done via AES. Note that the AES 
+implementation is provided by the pycrypto module. 
 https://www.dlitz.net/software/pycrypto/
 
 Copyright 2013 Mattis Zbigniew Haase
 
 This file is part of Dropcrypt.
 
-    Dropcrypt is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    Dropcrypt is free software: you can redistribute it
+    and/or modify it under the terms of the GNU 
+    General Public License as published by the Free 
+    Software Foundation, either version 3 of the 
+    License, or (at your option) any later version.
 
-    Dropcrypt is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Dropcrypt is distributed in the hope that it will be
+    useful, but WITHOUT ANY WARRANTY; without 
+    even the implied warranty of MERCHANTABILITY 
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Dropcrypt.  If not, see <http://www.gnu.org/licenses/>.
-
-It makes use of the pycrypto module, which is released under
-public domain
-
-It further makes use of some code snippets from 
-http://eli.thegreenplace.net/programs-and-code/ which are released
-under public domain
+    You should have received a copy of the GNU 
+    General Public License along with Dropcrypt.  
+    If not, see <http://www.gnu.org/licenses/>.
 """
 
-import os, random, struct, configuration
+import os
+import random
+import struct
+
 from Crypto.Cipher import AES
-from Crypto.Hash import MD5
-def Encrypt (chunksize, key, local, dropbox):
+
+import configuration
+
+def Encrypt (source, destination,  key):
     """  
-    Encypts the file "source/filename" with AES and saves it to 
-    "destination/filename". 
+    Encypts the file "source/filename" with AES and 
+    saves it to "destination/filename". 
     
-    unfortunately the pycrypto module doesnt have good doc. 
-    so thanks to eli bendersky
+    unfortunately the pycrypto module doesnt have 
+    good doc. so thanks to eli bendersky
     http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto/
     
-    chunksize: to speed up encryption, files are seperated into 
-    binary chunks of chunksze bytes, then each chunk is encrypted
-    must be divisible by 16
+    source: complete path of the source file
     
-    key: the AES key that should be 16,24 or 32 bytes long. 
+    destination: complete path of the destination file
     
-    local: complete path of the source file
-    
-    dropbox: complete path of the destination file
+    key: 32 byte string
     """
     filesize = os.path.getsize(local)
-    init_vector= ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-    encryptobject = AES.new(key, AES.MODE_CBC, init_vector)
+    init_vector= ''.join(chr(random.randint(0, 0xFF))
+                                            for i in range(16))
+    encryptobject = AES.new(
+                            key, AES.MODE_CBC, init_vector)
     with open(local, 'rb') as localobj:
         with open(dropbox, 'wb') as dropboxobj:
             dropboxobj.write(struct.pack('<Q', filesize))
             dropboxobj.write(init_vector)
             while True:
-                chunk = localobj.read(chunksize)
+                chunk = localobj.read(
+                                      configuration.chunksize)
                 if len(chunk) == 0:
                     break
                 elif len(chunk) % 16 != 0:
                     chunk += ' ' * (16 - len(chunk) % 16)
 
-                dropboxobj.write(encryptobject.encrypt(chunk))
+                dropboxobj.write(
+                                 encryptobject.encrypt(chunk))            
             
-            
-def Decrypt (chunksize, key, local, dropbox):
+def Decrypt (source, destination, key):
     """decrypts a file using AES.
     variables like encrypt
-    so dropbox is for example /Dropbox/file
     """
     with open(dropbox, 'rb') as dropboxobj:
-        filesize = struct.unpack('<Q', dropboxobj.read(struct.calcsize('Q')))[0]
+        filesize = (struct.unpack('<Q', 
+               dropboxobj.read(struct.calcsize('Q')))[0])
         init_vector = dropboxobj.read(16)
-        decryptobject = AES.new(key, AES.MODE_CBC, init_vector)
+        decryptobject = AES.new(key,
+                                AES.MODE_CBC, init_vector)
         with open(local, 'wb') as localobj:
             while True:
-                chunk = dropboxobj.read(chunksize)
+                chunk = dropboxobj.read(
+                                        configuration.chunksize)
                 if len(chunk) == 0:
                     break
-                localobj.write(decryptobject.decrypt(chunk))
+                localobj.write(
+                               decryptobject.decrypt(chunk))
             localobj.truncate(filesize)
-
-def Hash (path):
-    """returns MD5 hash of a file, doesnt need to be secure, hence MD5"""
-    hashor = MD5.new()
-    chunksize=8192
-    with open (path,  rb) as file:
-        while True:
-            chunk = file.read(chunk_size)
-            if len(chunk) == 0:
-                break
-            hashor.update(chunk)
-    return hashor.hexdigest()
